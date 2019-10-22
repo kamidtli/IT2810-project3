bb75f43686a4abb32fa6bf087d6934bc6d257fd2
Kim <kmidtlid3@gmail.com>
2019-10-22 20:44:17 +0200

    Removed compilation errors

--- src/server/schema/schemaV2.js
+++ src/server/schema/schemaV2.js
@@ -47,81 +47,85 @@ const resolvers = {
   Query: {
     allMovies: async () => await Movie.find(),
     movie: async (root, { _id }) => await Movie.findById(_id),
-    findMoviesBasedOnTitle: async (root, { title, pagination, skip, sort }) =>
-      await Movie.find({ title: { $regex: title, $options: 'i' } })
-        .skip(skip)
-        .limit(pagination)
-        .sort(sort),
+    findMoviesBasedOnTitle: async (root, {
+      title, pagination, skip, sort,
+    }) => await Movie.find({ title: { $regex: title, $options: 'i' } })
+      .skip(skip)
+      .limit(pagination)
+      .sort(sort),
     findMoviesBasedOnDirector: async (
       root,
-      { director, pagination, skip, sort }
-    ) =>
-      await Movie.find({ directors: { $regex: director, $options: 'i' } })
-        .skip(skip)
-        .limit(pagination)
-        .sort(sort),
-    findMoviesBasedOnYear: async (root, { year, pagination, skip, sort }) =>
-      await Movie.find({ year })
-        .skip(skip)
-        .limit(pagination)
-        .sort(sort),
+      {
+        director, pagination, skip, sort,
+      },
+    ) => await Movie.find({ directors: { $regex: director, $options: 'i' } })
+      .skip(skip)
+      .limit(pagination)
+      .sort(sort),
+    findMoviesBasedOnYear: async (root, {
+      year, pagination, skip, sort,
+    }) => await Movie.find({ year })
+      .skip(skip)
+      .limit(pagination)
+      .sort(sort),
     findMoviesBasedOnYearRange: async (
       root,
-      { min, max, pagination, skip, sort }
-    ) =>
-      await Movie.find({ year: { $lte: max, $gte: min } })
-        .skip(skip)
-        .limit(pagination)
-        .sort(sort),
-    findMoviesBasedOnGenre: async (root, { genre, pagination, skip, sort }) =>
-      await Movie.find({ genres: { $regex: genre, $options: 'i' } })
-        .skip(skip)
-        .limit(pagination)
-        .sort(sort),
+      {
+        min, max, pagination, skip, sort,
+      },
+    ) => await Movie.find({ year: { $lte: max, $gte: min } })
+      .skip(skip)
+      .limit(pagination)
+      .sort(sort),
+    findMoviesBasedOnGenre: async (root, {
+      genre, pagination, skip, sort,
+    }) => await Movie.find({ genres: { $regex: genre, $options: 'i' } })
+      .skip(skip)
+      .limit(pagination)
+      .sort(sort),
     filterMovies: async (
       root,
-      { searchValue, yearRange, ratingRange, genre, pagination, skip, sort }
-    ) =>
-      await Movie.find({
-        $or: [
-          { title: { $regex: searchValue, $options: 'i' } },
-          { directors: { $regex: searchValue, $options: 'i' } }
-        ],
-        genres: { $regex: genre, $options: 'i' },
-        year: { $lte: yearRange[1], $gte: yearRange[0] },
-        'imdb.rating': { $lte: ratingRange[1], $gte: ratingRange[0] }
-      })
-        .skip(skip)
-        .limit(pagination)
-        .sort(sort)
+      {
+        searchValue, yearRange, ratingRange, genre, pagination, skip, sort,
+      },
+    ) => await Movie.find({
+      $or: [
+        { title: { $regex: searchValue, $options: 'i' } },
+        { directors: { $regex: searchValue, $options: 'i' } },
+      ],
+      genres: { $regex: genre, $options: 'i' },
+      year: { $lte: yearRange[1], $gte: yearRange[0] },
+      'imdb.rating': { $lte: ratingRange[1], $gte: ratingRange[0] },
+    })
+      .skip(skip)
+      .limit(pagination)
+      .sort(sort),
+    findImdbRatingPerYear: async (root, { year }) => await Movie.find({ year }).then(
+      (data) => data
+        .reduce((obj, item) => {
+          if (
+            obj.find((o, i) => {
+              if (parseFloat(o.x) === parseFloat(item.imdb.rating)) {
+                obj[i] = {
+                  x: parseFloat(o.x),
+                  y: obj[i].y + 1,
+                };
+                return true; // stop searching
+              }
+            })
+          ) {
+          } else {
+            obj.push({
+              x: parseFloat(item.imdb.rating),
+              y: 1,
+            });
+          }
+          return obj;
+        }, [])
+        .sort((movie1, movie2) => (movie1.x > movie2.x ? 1 : -1)),
+      (error) => console.log(error),
+    ),
   },
-  findImdbRatingPerYear: async (root, { year }) =>
-    await Movie.find({ year }).then(
-      data =>
-        data
-          .reduce((obj, item) => {
-            if (
-              obj.find((o, i) => {
-                if (parseFloat(o.x) === parseFloat(item.imdb.rating)) {
-                  obj[i] = {
-                    x: parseFloat(o.x),
-                    y: obj[i].y + 1
-                  };
-                  return true; // stop searching
-                }
-              })
-            ) {
-            } else {
-              obj.push({
-                x: parseFloat(item.imdb.rating),
-                y: 1
-              });
-            }
-            return obj;
-          }, [])
-          .sort((movie1, movie2) => (movie1.x > movie2.x ? 1 : -1)),
-      error => console.log(error)
-    )
 };
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
